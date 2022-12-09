9d06095c7c77e6f196755d7c642009ad384b7420
Loise <loise.kimani@student.moringaschool.com>
2022-12-09 05:21:00 +0300

    Cmpleted Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  
+  let form = document.querySelector("form")
+  form.addEventListener("submit", (event)=>{
+      event.preventDefault()
+      handleToDo(event.target.newTaskDescription.value)
+      form.reset()
+  } )
 });
+
+function handleToDo(todo){
+ 
+  
+ let li = document.createElement("li")
+ 
+
+  document.getElementById("tasks").appendChild(li)
+ 
+ 
+ btn.textContent= "Do it."
+
+ btn.addEventListener("click", handleDelete)
+ 
+ li.textContent = `${todo}`
+ 
+ li.appendChild(buton)
+  let btn = document.createElement("button") 
+
+}
+
+ function handleDelete(event){
+  event.target.parentNode.remove()
+ }
+;
